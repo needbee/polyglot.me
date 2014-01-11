83121d0e95638577abce032ab54893bdfaa319f1
Josh Justice <josh@need-bee.com>
2014-01-11 10:20:15 -0500

    initial commit, transferring code from svn

---
+++ includes/js/utils.js
@@ -0,0 +1,28 @@
+var utils = utils || {};
+utils.one_or_many = function( maybeArray, callback ) {
+	if( _.isArray(maybeArray) ) {
+		_.each( maybeArray, callback );
+	} else {
+		callback( maybeArray );
+	}
+}
+
+utils.sample_link = function( urlString ) {
+	if( !urlString ) {
+		return '';
+	}
+
+	if( -1 != urlString.indexOf('jsfiddle.net') ) {
+		label = 'jsfiddle';
+	} else if( -1 != urlString.indexOf('gist.github.com')) {
+		label = 'gist';
+	} else {
+		label = 'sample';
+	}
+
+	return '<a href="'+urlString+'" target="_blank">'+label+'</a>';
+}
+
+utils.has_flash = function() {
+	return navigator.mimeTypes["application/x-shockwave-flash"] != undefined;
+}
